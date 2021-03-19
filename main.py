25763bdac038d603740ffcf3d011f8a150bb4dc0
Aamna Farooq <aaf.zakaria@gmail.com>
2021-03-19 16:01:45 -0400

    add in ec to main

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from generateMine import generateMinesGrid
 from visualizeBoard import visualizeBoard
 from basicagent import basicAgent, finalScore
 from improvedAgent import improvedAgent
+from globalInformation import improvedAgent
+from betterDecisions import betterImprovedAgent
 
 def main():
 
     dimension = 20
-    mines = 30
+    mines = 60
     grid = generateMinesGrid(dimension, mines)
     # print(grid)
     # grid = [[2, 'M', 'M', 'M', 'M'], ['M', 3, 4, 5, 'M'], [2, 2, 2, 'M', 2], [2, 'M', 3, 2, 1], ['M', 3, 'M', 1, 0]]
     # grid = [[0, 1, 'M', 1, 0], [1, 2, 2, 1, 0], [3, 'M', 2, 0, 0], ['M', 'M', 3, 2, 2], ['M', 3, 2, 'M', 'M']]
     # usergrid = basicAgent(grid)
-    usergrid = improvedAgent(grid)
+    # usergrid = improvedAgent(grid)
+    # usergrid= improvedAgent(grid, mines)
+    usergrid = betterImprovedAgent(grid)
     visualizeBoard(usergrid, "final basic agent")
     visualizeBoard(grid, "actual grid")
     score = finalScore(grid, usergrid)
